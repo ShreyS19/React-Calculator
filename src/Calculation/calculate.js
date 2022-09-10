0a3c14e620c2e0b7c3b01779f7f344d0f7a9afd8
ShreyS@19 <sshreyash19@gmail.com>
2022-09-10 20:37:59 +0530

    Final code for calculator app

--- src/Calculation/calculate.js
+++ src/Calculation/calculate.js
@@ -1,3 +1,4 @@
+import Big from "big-js";
 import React from "react";
 import isNumber from "./isNumber";
 import operate from "./operate";
@@ -11,8 +12,54 @@ export default function calculate(obj, name) {
   }
 
   if (isNumber(name)) {
+    if (name == "0" && obj.next == "0") return {};
+
+    if (obj.operation) {
+      if (obj.next) {
+        return { next: obj.next + name };
+      }
+      return { next: name };
+    }
+    if (obj.next) {
+      let nextVal = obj.next == "0" ? name : obj.next + name;
+      return { next: nextVal, total: null };
+    }
     return {
-      next: (obj.next || "") + name,
+      next: name,
+      total: null,
+    };
+  }
+
+  if (name === "%") {
+    if (obj.operation && obj.next) {
+      let percentVal = Big(obj.next * obj.total)
+        .div(Big("100"))
+        .toString();
+      let result = operate(obj.total, percentVal, obj.operation);
+
+      return {
+        total: result,
+        next: null,
+        operation: null,
+      };
+    }
+    if (obj.next) {
+      return {
+        total: Big(obj.next).div(Big("100")).toString(),
+      };
+    }
+    return {};
+  }
+
+  if (name === ".") {
+    if (obj.next) {
+      if (obj.next.includes(".")) return {};
+      return {
+        next: obj.next + ".",
+      };
+    }
+    return {
+      next: "0.",
     };
   }
 
@@ -24,8 +71,22 @@ export default function calculate(obj, name) {
         operation: null,
       };
     } else {
-      return {};
+      return obj;
     }
+  }
+
+  if (name === "+/-") {
+    if (obj.next) {
+      return {
+        next: "-" + parseFloat(obj.next).toString(),
+      };
+    }
+    if (obj.total) {
+      return {
+        total: "-" + parseFloat(obj.total).toString(),
+      };
+    }
+    return {};
   }
 
   if (obj.operation) {
